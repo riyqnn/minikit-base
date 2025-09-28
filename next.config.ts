f87e68c11b15d8556177069931b70a7d93ce83e5
riyan <riyanarhm28@gmail.com>
2025-09-28 13:13:58 +0700

    redeploy v3

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/.well-known/farcaster.json',
-        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/01998e94-c322-08ac-f54d-f07e1475e8ab',
-        permanent: true,
-      },
-    ]
-  },
-}
+import type { NextConfig } from "next";
 
-module.exports = nextConfig
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
+
+export default nextConfig;
