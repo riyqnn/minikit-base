6550cf979b50242a9d2721f590a0ca0fd1013138
riyan <riyanarhm28@gmail.com>
2025-09-28 11:45:26 +0700

    fixing little issue

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
-@type {import('next').NextConfig}
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/.well-known/farcaster.json",
-        destination: "https://api.farcaster.xyz/miniapps/hosted-manifest/01998e94-c322-08ac-f54d-f07e1475e8ab",
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/01998e94-c322-08ac-f54d-f07e1475e8ab',
         permanent: true,
       },
-    ];
+    ]
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
